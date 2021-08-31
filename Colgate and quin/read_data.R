60eca8ed26db96ad410db6c1b1317695674afb4e
dongq-web <61438458+dongq-web@users.noreply.github.com>
2021-08-30 21:32:20 -0400

    Add files via upload

---
+++ Colgate and quin/read_data.R
@@ -0,0 +1,7 @@
+Col_1 <- read.csv("C:\\Users\\lenovo\\Desktop\\hockey\\Colgate and quin\\Colgate P1.csv.csv")
+col_2 <- read.csv("C:\\Users\\lenovo\\Desktop\\hockey\\Colgate and quin\\Colgate P2.csv.csv")
+col_3 <- read.csv("C:\\Users\\lenovo\\Desktop\\hockey\\Colgate and quin\\Colgate P3.csv.csv")
+
+qpac_1 <- read.csv("C:\\Users\\lenovo\\Desktop\\hockey\\Colgate and quin\\QPac P1.csv.csv")
+qpac_2 <- read.csv("C:\\Users\\lenovo\\Desktop\\hockey\\Colgate and quin\\QPac P2.csv.csv")
+qpac_3 <- read.csv("C:\\Users\\lenovo\\Desktop\\hockey\\Colgate and quin\\QPac P3.csv.csv")
